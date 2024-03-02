c26f62332693c5562b0468cd57ca4a83cbc03585
emadeldinadel2022 <567emad@gmail.com>
2024-03-02 10:12:13 +0200

    python venv

--- SQL/Data_Exploration.sql
+++
@@ -1,9 +0,0 @@
---Total counts per invoice, stocks, customers
-SELECT 
-    COUNT(*) AS total_rows,
-    COUNT(DISTINCT invoice) AS invoice_counts,
-    COUNT(DISTINCT stockcode) AS stock_counts,
-    COUNT(DISTINCT customer_id) AS customers_counts
-FROM tableretail;
-
-
